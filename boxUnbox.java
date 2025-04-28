fd56ac1f1439214d3448cfc7bbe02fc78dbe88ff
jagath-sajjan <jagathsajjan097@gmail.com>
2025-04-28 07:24:51 +0530

    Create boxUnbox.java

---
+++ boxUnbox.java
@@ -0,0 +1,11 @@
+public class bub {
+    public static void main(String[] args) {
+        int a = 50;
+        Integer a1 = new Integer(a);
+        Integer a2 = 5;
+        int b = a2;
+        System.out.println(a1 + " " + a2);
+        int c = 1;
+        System.out.println(c);
+    }
+}
